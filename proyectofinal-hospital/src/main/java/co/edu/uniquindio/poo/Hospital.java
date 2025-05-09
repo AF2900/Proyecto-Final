6fa534e726ef29978c13385760496264d5b257c9
Gonzo024 <santiagofutbol2007@gmail.com>
2025-05-08 19:06:38 -0500

    Cree la clase Hospital

---
+++ proyectofinal-hospital/src/main/java/co/edu/uniquindio/poo/Hospital.java
@@ -0,0 +1,36 @@
+package co.edu.uniquindio.poo;
+
+import java.util.LinkedList;
+
+public class Hospital {
+    private String nombre;
+    private String nit;
+    private LinkedList<Cita> listCitas;
+    private LinkedList<Medico> listMedicos;
+
+
+    public Hospital(String nombre, String nit) {
+        this.nombre = nombre;
+        this.nit = nit;
+        this.listCitas = new LinkedList<>();
+        this.listMedicos = new LinkedList<>();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNit() {
+        return nit;
+    }
+
+    public void setNit(String nit) {
+        this.nit = nit;
+    }
+    
+
+}
--- proyectofinal-hospital/src/main/java/co/edu/uniquindio/poo/Medico.java
+++ proyectofinal-hospital/src/main/java/co/edu/uniquindio/poo/Medico.java
@@ -1,5 +1,7 @@
 package co.edu.uniquindio.poo;
 
-public class Medico {
-    
+public class Medico extends Persona {
+
+
+
 }
