697c9bf9489bc8e7f865af3113f1d88d1564f77f
TheEternalCode <imjustabluejay@gmail.com>
2025-05-22 15:56:29 -0500

    Creación del segundo test, se requiere atención, error de debugguer

--- proyecto-Final-fx/src/test/java/co/edu/uniquindio/poo/proyectofinal/HospitalTest.java
+++ proyecto-Final-fx/src/test/java/co/edu/uniquindio/poo/proyectofinal/HospitalTest.java
@@ -16,15 +16,21 @@ public class HospitalTest {
     Medico medico = new Medico("Dutch", "Vanderlinde", "090921", "900", "09213");
     HistorialMedico historial= new HistorialMedico("2221", null);
     Paciente paciente= new Paciente("Juan carlos", "Bodoque", "092861", "5532", "Deudas", "Casa 1", historial, null);
-    Cita cita= new Cita("1987", null, "17:40", medico, paciente);
+    Cita cita= new Cita("1987", LocalDate.of(2025, 5, 22), "17:40", medico, paciente);
+    
 //Test de creación de médicos
 @Test
 public void crearCitaTest(){
-    
     boolean resultado= hospital.crearCita(cita);
     assertTrue(resultado);
+}
 
-}    
+@Test
+public void eliminarCitaTest(){
+    hospital.crearCita(cita);
+    boolean borrado = hospital.eliminarCita(cita);
+    assertTrue(borrado);
+}
 
 
 }
